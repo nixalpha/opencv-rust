9d59c30e476375b06b21f00421ee6c577e445c36
nixalpha <n1xalphaa@gmail.com>
2022-03-08 17:34:05 -0800

    Multiple templates working, mulithreading todo

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,43 @@
 use anyhow::Result;
 use opencv::{core, highgui, imgcodecs, imgproc, prelude::*, videoio};
+use std::env;
+// use rayon;
+// use rayon::prelude::*;
+
+fn get_video_path() -> Result<String> {
+    let mut bin_dir = env::current_exe()?;
+    bin_dir.pop();
+    bin_dir.pop();
+    bin_dir.pop();
+    bin_dir.push("950_Video_Trim.mp4");
+    Ok(bin_dir.display().to_string())
+}
 
 fn main() -> Result<()> {
     highgui::named_window("window", highgui::WINDOW_FULLSCREEN)?;
-    let mut cam = videoio::VideoCapture::from_file("950_Video_Trim.mp4", videoio::CAP_ANY)?;
+    let mut cam = videoio::VideoCapture::from_file(&get_video_path()?, videoio::CAP_ANY)?;
     let mut frame = Mat::default();
 
-    let template = imgcodecs::imread("162.jpg", 0)?;
+    // let template = imgcodecs::imread("162.jpg", 0)?;
 
-    println!(
-        "{} {} {}",
-        template.rows(),
-        template.cols(),
-        template.dims()
-    );
+    let templates: [Mat; 9] = [imgcodecs::imread("photos/68.jpg", 0)?, 
+                                imgcodecs::imread("photos/68(2).jpg", 0)?, 
+                                imgcodecs::imread("photos/74.5.jpg", 0)?, 
+                                imgcodecs::imread("photos/84.jpg", 0)?,
+                                imgcodecs::imread("photos/115.jpg", 0)?,
+                                imgcodecs::imread("photos/162.jpg", 0)?,
+                                imgcodecs::imread("photos/209.jpg", 0)?,
+                                imgcodecs::imread("photos/256.jpg", 0)?,
+                                imgcodecs::imread("photos/303.jpg", 0)?];
+
+    // let pool = rayon::ThreadPoolBuilder::new().num_threads(4).build()?;
+
+    // println!(
+    //     "{} {} {}",
+    //     template.rows(),
+    //     template.cols(),
+    //     template.dims()
+    // );
 
     loop {
         cam.read(&mut frame)?;
@@ -22,16 +46,30 @@ fn main() -> Result<()> {
             imgproc::cvt_color(&frame, &mut gray, imgproc::COLOR_BGR2GRAY, 0)?;
             let mut gray_thresh = Mat::default();
             imgproc::threshold(&gray, &mut gray_thresh, 237.0, 255.0, 0)?;
+            
+            let mut match_result = Mat::default();
+            let mut highest_match: f64 = 0.0;
 
             // template matching
-            let mut match_result = Mat::default();
-            imgproc::match_template(
-                &gray_thresh,
-                &template,
-                &mut match_result,
-                imgproc::TemplateMatchModes::TM_CCOEFF_NORMED as i32,
-                &core::no_array(),
-            )?;
+            for template in &templates {
+                let mut temp_match_result = Mat::default();
+                imgproc::match_template(
+                    &gray_thresh,
+                    &template,
+                    &mut temp_match_result,
+                    imgproc::TemplateMatchModes::TM_CCOEFF_NORMED as i32,
+                    &core::no_array(),
+                )?;
+
+                let mut result_max: f64 = 0.0;
+
+                core::min_max_loc(&temp_match_result, None, Some(&mut result_max), None, None, &core::no_array())?;
+
+                if result_max > highest_match {
+                    highest_match = result_max;
+                    match_result = temp_match_result;
+                }
+            } 
 
             let mut max_val: f64 = 0.0;
 
